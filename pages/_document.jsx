b428488ce9e87971b1c589cf39dda946280e0ce4
Art San Diego <crtved.artsandiego@gmail.com>
2021-07-14 01:07:36 +0800

    Twitter card

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -26,20 +26,20 @@ export default class MyDocument extends Document {
           <script data-ad-client="ca-pub-7734586900480329" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           <meta name="title" content="Art San Diego | CRTVED" />
           <link rel="icon" type="image/png" href="/artsandiego-icon.png" />
-          <meta name="description" content="CRTVED /creative dev/ is a sole proprietorship that aims to provide a modern UI design approach and up-to-date website development methods for your digital needs."/>
+          <meta name="description" content="/kree·ay·tuhv/ | Crafting digital aesthetic experiences"/>
           <meta name="keywords" content="artsandiego, crtved, web, developer, front-end, freelance, website, designer, ui, ux, startup, small business"/>
 
           <meta property="og:type" content="website"/>
           <meta property="og:url" content="https://artsandiego.dev/"/>
-          <meta property="og:title" content="Art San Diego | CRTVED"/>
-          <meta property="og:description" content="CRTVED /creative dev/ is a sole proprietorship that aims to provide a modern UI design approach and up-to-date website development methods for your digital needs."/>
-          <meta property="og:image" content="/assets/images/artsandiego-crtved-social-image.png"/>
+          <meta property="og:title" content="CRTVED"/>
+          <meta property="og:description" content="/kree·ay·tuhv/ | Crafting digital aesthetic experiences"/>
+          <meta property="og:image" content="../public/assets/images/artsandiego-crtved-social-image.png"/>
 
           <meta property="twitter:card" content="summary_large_image"/>
           <meta property="twitter:url" content="https://artsandiego.dev/"/>
-          <meta property="twitter:title" content="Art San Diego | CRTVED"/>
-          <meta property="twitter:description" content="CRTVED /creative dev/ is a sole proprietorship that aims to provide a modern UI design approach and up-to-date website development methods for your digital needs."/>
-          <meta property="twitter:image" content="/assets/images/artsandiego-crtved-social-image.png" />
+          <meta property="twitter:title" content="CRTVED"/>
+          <meta property="twitter:description" content="/kree·ay·tuhv/ | Crafting digital aesthetic experiences"/>
+          <meta property="twitter:image" content="../public/assets/images/artsandiego-crtved-social-image.png" />
         </Head>
         <body>
           <Main />
