70721fa346441c9538e5d6fbde494f30db6d6b01
Art San Diego <crtved.artsandiego@gmail.com>
2021-07-26 20:23:45 +0800

    Links page

---
+++ pages/links.jsx
@@ -0,0 +1,74 @@
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { gsap } from "gsap";
+import Link from 'next/link';
+import { useQuery, gql } from '@apollo/client';
+import Image from 'next/image';
+
+const LOGO = gql`
+  query GetLogoNavigationLinks {
+    logo {
+      image {
+        url
+        alternativeText
+      }
+    }
+  }
+`;
+
+const Links = () => {
+  useEffect(() => {
+     /* cursor follow animation */
+     let cursorContainer = document.querySelector(".cursor-container");
+     let moveCircle = (e) => {
+       gsap.to(cursorContainer, 0.3, {
+         css: {
+           left: e.pageX,
+           top: e.pageY
+         }
+       });
+     }
+    window.addEventListener('mousemove', moveCircle);
+    
+    const selectAllUnique = (element, trigger) => {
+      return document.querySelectorAll(element).forEach(e => {
+        e.addEventListener('mousemove', () => {cursorContainer.classList.add(trigger);})
+        e.addEventListener('mouseleave', () => {cursorContainer.classList.remove(trigger);})
+      });
+    }
+    selectAllUnique('.alt', 'active-alt');
+  })
+
+  const { loading, error, data } = useQuery(LOGO);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :( Error Message: {error}</p>
+
+  return (
+    <>
+      <Head>
+        <title>Links | Art San Diego</title>
+      </Head>
+      <div className="cursor-container"><div className="cursor"></div></div>
+      <div className="film-grain"></div>
+      <div className="vertical-lines"></div>
+      <section className="h-screen w-3/4 mx-auto flex justify-center items-center">
+        <div className="flex flex-col text-center">
+          <Image src={data.logo.image.url} alt={data.logo.image.alternativeText} height="58px" width="52px" />
+          <p className="text-white font-montserrat text-25 mt-8 z-10">CRTVED</p>
+          <p className="text-white font-montserrat text-25 z-10">Pronounced /kree·ay·tuhv/ Crafting aesthetic experiences for the web. 🔥</p>
+          <div className="flex flex-col mt-8">
+            <Link href="https://artsandiego.dev"><a className="alt text-secondary font-montserrat text-25 font-bold uppercase my-4 z-10 bg-primary py-4 px-32 rounded-lg" rel="noopener noreferrer" target="_blank">Portfolio</a></Link>
+            <Link href="https://instagram.com/_crt.ved"><a className="alt text-secondary font-montserrat text-25 font-bold uppercase my-4 z-10 bg-primary py-4 px-32 rounded-lg" rel="noopener noreferrer" target="_blank">Instagram</a></Link>
+            <Link href="https://behance.net/crtved"><a className="alt text-secondary font-montserrat text-25 font-bold uppercase my-4 z-10 bg-primary py-4 px-32 rounded-lg" rel="noopener noreferrer" target="_blank">Behance</a></Link>
+            <Link href="https://github.com/crtved"><a className="alt text-secondary font-montserrat text-25 font-bold uppercase my-4 z-10 bg-primary py-4 px-32 rounded-lg" rel="noopener noreferrer" target="_blank">Github</a></Link>
+            <Link href="https://fb.com/creativedeveloperph"><a className="alt text-secondary font-montserrat text-25 font-bold uppercase my-4 z-10 bg-primary py-4 px-32 rounded-lg" rel="noopener noreferrer" target="_blank">Facebook Page</a></Link>
+            <Link href="https://linkedin.com/in/artsandiego"><a className="alt text-secondary font-montserrat text-25 font-bold uppercase my-4 z-10 bg-primary py-4 px-32 rounded-lg" rel="noopener noreferrer" target="_blank">LinkedIn</a></Link>
+          </div>
+        </div>
+      </section>
+    </>
+   );
+}
+ 
+export default Links;
